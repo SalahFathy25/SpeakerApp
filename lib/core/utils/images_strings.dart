4b3157c3c8c67d67a3ec8d627abed3c2f03f3752
Salah Fathy <SalahFathy@gmail.com>
2025-04-17 22:27:08 +0200

    first commit

--- lib/core/utils/images_strings.dart
+++ lib/core/utils/images_strings.dart
@@ -1,65 +1,224 @@
-class ImagesStrings {
-  // ===== png images =======
-  // App
-  static const String findFriendsDarkPng =
-      'assets/images/png/app/dark_find_friends.png';
-  static const String findFriendsLightPng =
-      'assets/images/png/app/light_find_friends.png';
-  // App Logos
-  static const String blackHelloLogoPng =
-      'assets/images/png/app_logos/black_hello_logo.png';
-  static const String whiteHelloLogoPng =
-      'assets/images/png/app_logos/white_hello_logo.png';
-  static const String blackLogoPng =
-      'assets/images/png/app_logos/black_logo.png';
-  static const String whiteLogoPng =
-      'assets/images/png/app_logos/white_logo.png';
-  static const String iconPng = 'assets/images/png/app_logos/icon.png';
-  static const String logo80Png = 'assets/images/png/app_logos/Logo80_80.png';
-  // Onboarding
-  static const String darkOnboardingPng =
-      'assets/images/png/onboarding/dark_onboarding.png';
-  static const String lightOnboardingPng =
-      'assets/images/png/onboarding/light_onboarding.png';
-  static const String lightWelcomePng =
-      'assets/images/png/onboarding/light_welcome.png';
-  static const String darkWelcomePng =
-      'assets/images/png/onboarding/dark_welcome.png';
-  // Register
-  static const String darkChooseLevelPng =
-      'assets/images/png/register/dark_choose_level.png';
-  static const String lightChooseLevelPng =
-      'assets/images/png/register/light_choose_level.png';
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
 
-  // ===== svg images =======
-  // App
-  static const String findFriendsDarkSvg =
-      'assets/images/png/app/dark_find_friends.svg';
-  static const String findFriendsLightSvg =
-      'assets/images/png/app/light_find_friends.svg';
-  // App Logos
-  static const String blackHelloLogoSvg =
-      'assets/images/png/app_logos/black_hello_logo.svg';
-  static const String whiteHelloLogoSvg =
-      'assets/images/png/app_logos/white_hello_logo.svg';
-  static const String blackLogoSvg =
-      'assets/images/png/app_logos/black_logo.svg';
-  static const String whiteLogoSvg =
-      'assets/images/png/app_logos/white_logo.svg';
-  static const String iconSvg = 'assets/images/png/app_logos/icon.svg';
-  static const String logo80Svg = 'assets/images/png/app_logos/Logo80_80.svg';
-  // Onboarding
-  static const String darkOnboardingSvg =
-      'assets/images/png/onboarding/dark_onboarding.svg';
-  static const String lightOnboardingSvg =
-      'assets/images/png/onboarding/light_onboarding.svg';
-  static const String lightWelcomeSvg =
-      'assets/images/png/onboarding/light_welcome.svg';
-  static const String darkWelcomeSvg =
-      'assets/images/png/onboarding/dark_welcome.svg';
-  // Register
-  static const String darkChooseLevelSvg =
-      'assets/images/png/register/dark_choose_level.svg';
-  static const String lightChooseLevelSvg =
-      'assets/images/png/register/light_choose_level.svg';
+  /// Assets for imagesPngAppDarkFindFriends
+  /// assets/images/png/app/dark_find_friends.png
+  static const String imagesPngAppDarkFindFriends =
+      "assets/images/png/app/dark_find_friends.png";
+
+  /// Assets for imagesPngAppLightFindFriends
+  /// assets/images/png/app/light_find_friends.png
+  static const String imagesPngAppLightFindFriends =
+      "assets/images/png/app/light_find_friends.png";
+
+  /// Assets for imagesPngAppLogosBlackHelloLogo
+  /// assets/images/png/app_logos/black_hello_logo.png
+  static const String imagesPngAppLogosBlackHelloLogo =
+      "assets/images/png/app_logos/black_hello_logo.png";
+
+  /// Assets for imagesPngAppLogosBlackLogo
+  /// assets/images/png/app_logos/black_logo.png
+  static const String imagesPngAppLogosBlackLogo =
+      "assets/images/png/app_logos/black_logo.png";
+
+  /// Assets for imagesPngAppLogosIcon
+  /// assets/images/png/app_logos/icon.png
+  static const String imagesPngAppLogosIcon =
+      "assets/images/png/app_logos/icon.png";
+
+  /// Assets for imagesPngAppLogosLogo8080
+  /// assets/images/png/app_logos/Logo80_80.png
+  static const String imagesPngAppLogosLogo8080 =
+      "assets/images/png/app_logos/Logo80_80.png";
+
+  /// Assets for imagesPngAppLogosWhiteHelloLogo
+  /// assets/images/png/app_logos/white_hello_logo.png
+  static const String imagesPngAppLogosWhiteHelloLogo =
+      "assets/images/png/app_logos/white_hello_logo.png";
+
+  /// Assets for imagesPngAppLogosWhiteLogo
+  /// assets/images/png/app_logos/white_logo.png
+  static const String imagesPngAppLogosWhiteLogo =
+      "assets/images/png/app_logos/white_logo.png";
+
+  /// Assets for imagesPngOnboardingDarkOnboarding
+  /// assets/images/png/onboarding/dark_onboarding.png
+  static const String imagesPngOnboardingDarkOnboarding =
+      "assets/images/png/onboarding/dark_onboarding.png";
+
+  /// Assets for imagesPngOnboardingDarkWelcome
+  /// assets/images/png/onboarding/dark_welcome.png
+  static const String imagesPngOnboardingDarkWelcome =
+      "assets/images/png/onboarding/dark_welcome.png";
+
+  /// Assets for imagesPngOnboardingLightOnboarding
+  /// assets/images/png/onboarding/light_onboarding.png
+  static const String imagesPngOnboardingLightOnboarding =
+      "assets/images/png/onboarding/light_onboarding.png";
+
+  /// Assets for imagesPngOnboardingLightWelcome
+  /// assets/images/png/onboarding/light_welcome.png
+  static const String imagesPngOnboardingLightWelcome =
+      "assets/images/png/onboarding/light_welcome.png";
+
+  /// Assets for imagesPngRegisterDarkChooseLevel
+  /// assets/images/png/register/dark_choose_level.png
+  static const String imagesPngRegisterDarkChooseLevel =
+      "assets/images/png/register/dark_choose_level.png";
+
+  /// Assets for imagesPngRegisterLightChooseLevel
+  /// assets/images/png/register/light_choose_level.png
+  static const String imagesPngRegisterLightChooseLevel =
+      "assets/images/png/register/light_choose_level.png";
+
+  /// Assets for imagesSvgAppDarkFindFriends
+  /// assets/images/svg/app/dark_find_friends.svg
+  static const String imagesSvgAppDarkFindFriends =
+      "assets/images/svg/app/dark_find_friends.svg";
+
+  /// Assets for imagesSvgAppLightFindFriends
+  /// assets/images/svg/app/light_find_friends.svg
+  static const String imagesSvgAppLightFindFriends =
+      "assets/images/svg/app/light_find_friends.svg";
+
+  /// Assets for imagesSvgAppLogosBlackHelloLogo
+  /// assets/images/svg/app_logos/black_hello_logo.svg
+  static const String imagesSvgAppLogosBlackHelloLogo =
+      "assets/images/svg/app_logos/black_hello_logo.svg";
+
+  /// Assets for imagesSvgAppLogosBlackLogo
+  /// assets/images/svg/app_logos/black_logo.svg
+  static const String imagesSvgAppLogosBlackLogo =
+      "assets/images/svg/app_logos/black_logo.svg";
+
+  /// Assets for imagesSvgAppLogosIcon
+  /// assets/images/svg/app_logos/icon.svg
+  static const String imagesSvgAppLogosIcon =
+      "assets/images/svg/app_logos/icon.svg";
+
+  /// Assets for imagesSvgAppLogosLogo8080
+  /// assets/images/svg/app_logos/Logo80_80.svg
+  static const String imagesSvgAppLogosLogo8080 =
+      "assets/images/svg/app_logos/Logo80_80.svg";
+
+  /// Assets for imagesSvgAppLogosWhiteHelloLogo
+  /// assets/images/svg/app_logos/white_hello_logo.svg
+  static const String imagesSvgAppLogosWhiteHelloLogo =
+      "assets/images/svg/app_logos/white_hello_logo.svg";
+
+  /// Assets for imagesSvgAppLogosWhiteLogo
+  /// assets/images/svg/app_logos/white_logo.svg
+  static const String imagesSvgAppLogosWhiteLogo =
+      "assets/images/svg/app_logos/white_logo.svg";
+
+  /// Assets for imagesSvgAppTabBarDarkActiveBook
+  /// assets/images/svg/app/TabBar/dark/active_book.svg
+  static const String imagesSvgAppTabBarDarkActiveBook =
+      "assets/images/svg/app/TabBar/dark/active_book.svg";
+
+  /// Assets for imagesSvgAppTabBarDarkActiveChat
+  /// assets/images/svg/app/TabBar/dark/active_chat.svg
+  static const String imagesSvgAppTabBarDarkActiveChat =
+      "assets/images/svg/app/TabBar/dark/active_chat.svg";
+
+  /// Assets for imagesSvgAppTabBarDarkActiveProfile
+  /// assets/images/svg/app/TabBar/dark/active_profile.svg
+  static const String imagesSvgAppTabBarDarkActiveProfile =
+      "assets/images/svg/app/TabBar/dark/active_profile.svg";
+
+  /// Assets for imagesSvgAppTabBarDarkActiveVocabulary
+  /// assets/images/svg/app/TabBar/dark/active_vocabulary.svg
+  static const String imagesSvgAppTabBarDarkActiveVocabulary =
+      "assets/images/svg/app/TabBar/dark/active_vocabulary.svg";
+
+  /// Assets for imagesSvgAppTabBarDarkBook
+  /// assets/images/svg/app/TabBar/dark/book.svg
+  static const String imagesSvgAppTabBarDarkBook =
+      "assets/images/svg/app/TabBar/dark/book.svg";
+
+  /// Assets for imagesSvgAppTabBarDarkChat
+  /// assets/images/svg/app/TabBar/dark/chat.svg
+  static const String imagesSvgAppTabBarDarkChat =
+      "assets/images/svg/app/TabBar/dark/chat.svg";
+
+  /// Assets for imagesSvgAppTabBarDarkProfile
+  /// assets/images/svg/app/TabBar/dark/profile.svg
+  static const String imagesSvgAppTabBarDarkProfile =
+      "assets/images/svg/app/TabBar/dark/profile.svg";
+
+  /// Assets for imagesSvgAppTabBarDarkVocabulary
+  /// assets/images/svg/app/TabBar/dark/vocabulary.svg
+  static const String imagesSvgAppTabBarDarkVocabulary =
+      "assets/images/svg/app/TabBar/dark/vocabulary.svg";
+
+  /// Assets for imagesSvgAppTabBarLightActiveBook
+  /// assets/images/svg/app/TabBar/light/active_book.svg
+  static const String imagesSvgAppTabBarLightActiveBook =
+      "assets/images/svg/app/TabBar/light/active_book.svg";
+
+  /// Assets for imagesSvgAppTabBarLightActiveChat
+  /// assets/images/svg/app/TabBar/light/active_chat.svg
+  static const String imagesSvgAppTabBarLightActiveChat =
+      "assets/images/svg/app/TabBar/light/active_chat.svg";
+
+  /// Assets for imagesSvgAppTabBarLightActiveProfile
+  /// assets/images/svg/app/TabBar/light/active_profile.svg
+  static const String imagesSvgAppTabBarLightActiveProfile =
+      "assets/images/svg/app/TabBar/light/active_profile.svg";
+
+  /// Assets for imagesSvgAppTabBarLightActiveVocabulary
+  /// assets/images/svg/app/TabBar/light/active_vocabulary.svg
+  static const String imagesSvgAppTabBarLightActiveVocabulary =
+      "assets/images/svg/app/TabBar/light/active_vocabulary.svg";
+
+  /// Assets for imagesSvgAppTabBarLightBook
+  /// assets/images/svg/app/TabBar/light/Book.svg
+  static const String imagesSvgAppTabBarLightBook =
+      "assets/images/svg/app/TabBar/light/Book.svg";
+
+  /// Assets for imagesSvgAppTabBarLightChat
+  /// assets/images/svg/app/TabBar/light/Chat.svg
+  static const String imagesSvgAppTabBarLightChat =
+      "assets/images/svg/app/TabBar/light/Chat.svg";
+
+  /// Assets for imagesSvgAppTabBarLightProfile
+  /// assets/images/svg/app/TabBar/light/Profile.svg
+  static const String imagesSvgAppTabBarLightProfile =
+      "assets/images/svg/app/TabBar/light/Profile.svg";
+
+  /// Assets for imagesSvgAppTabBarLightVocabulary
+  /// assets/images/svg/app/TabBar/light/Vocabulary.svg
+  static const String imagesSvgAppTabBarLightVocabulary =
+      "assets/images/svg/app/TabBar/light/Vocabulary.svg";
+
+  /// Assets for imagesSvgOnboardingDarkOnboarding
+  /// assets/images/svg/onboarding/dark_onboarding.svg
+  static const String imagesSvgOnboardingDarkOnboarding =
+      "assets/images/svg/onboarding/dark_onboarding.svg";
+
+  /// Assets for imagesSvgOnboardingDarkWelcome
+  /// assets/images/svg/onboarding/dark_welcome.svg
+  static const String imagesSvgOnboardingDarkWelcome =
+      "assets/images/svg/onboarding/dark_welcome.svg";
+
+  /// Assets for imagesSvgOnboardingLightOnboarding
+  /// assets/images/svg/onboarding/light_onboarding.svg
+  static const String imagesSvgOnboardingLightOnboarding =
+      "assets/images/svg/onboarding/light_onboarding.svg";
+
+  /// Assets for imagesSvgOnboardingLightWelcome
+  /// assets/images/svg/onboarding/light_welcome.svg
+  static const String imagesSvgOnboardingLightWelcome =
+      "assets/images/svg/onboarding/light_welcome.svg";
+
+  /// Assets for imagesSvgRegisterDarkChooseLevel
+  /// assets/images/svg/register/dark_choose_level.svg
+  static const String imagesSvgRegisterDarkChooseLevel =
+      "assets/images/svg/register/dark_choose_level.svg";
+
+  /// Assets for imagesSvgRegisterLightChooseLevel
+  /// assets/images/svg/register/light_choose_level.svg
+  static const String imagesSvgRegisterLightChooseLevel =
+      "assets/images/svg/register/light_choose_level.svg";
 }
